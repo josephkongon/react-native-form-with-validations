fe4e9bd730ebdf211563fe4bca06b4e6ff20787b
josephnyx <josephkongon@gmail.com>
2024-08-29 22:45:33 +0300

    export fix

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,14 @@
-export * from './Form';
-export * from './components';
+export {default as Form} from './Form';
+export {default as FormItem} from './FormItems/FormItem';
+export {default as FormInput} from './FormItems/FormInput';
+export {FormSwitch} from './FormItems/FormSwitch';
+export {default as FormDatePicker} from './FormItems/FormDatePicker';
+export {default as FormCheckbox} from './FormItems/FormCheckbox';
+export {default as FormRadioGroup} from './FormItems/FormRadioGroup';
+export {default as FormSlider} from './FormItems/FormSlider';
+export {default as FormPickerSelect} from './FormItems/FormPickerSelect';
+export {default as DropdownSelectForm} from './FormItems/FromDropdownSelect';
+export {default as FormRangeSlider} from './FormItems/FromRangeSlider';
+export {default as FormMultiSelect} from './FormItems/FormMultiSelect';
+
 export * from './hooks';
